a8bc217d856bc31db3bf132df7c2a8b7dc8f07bc
Ryo Fujiwara <jjcproject@icloud.com>
2021-05-05 23:26:17 +0900

    test

---
+++ nuxt.config.js
@@ -0,0 +1,63 @@
+import axios from 'axios'
+
+
+
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'microcms-nuxt-jamstack-blog',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  // modules: [
+  // ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+
+  generate: {
+    async routes() {
+      const pages = await axios
+        .get('', {
+          headers: { 'X-API-KEY': '78f8f871-a021-4bda-ad62-887f8a5d39fc' }
+        })
+        .then((res) =>
+          res.data.contents.map((content) => ({
+            route: `/${content.id}`,
+            payload: content
+          }))
+        )
+      return pages
+    }
+  }
+}
---
+++ pages/_slug/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ title }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+    <div class="post" v-html="body"></div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      `https://jjcproject.microcms.io/api/v1/blogme/${params.slug}`,
+      {
+        headers: { 'X-API-KEY': '78f8f871-a021-4bda-ad62-887f8a5d39fc' }
+      }
+    )
+    return data
+  }
+}
+</script>
+
+<style scoped>
+.main {
+	 width: 960px;
+	 margin: 0 auto;
+}
+ .title {
+	 margin-bottom: 20px;
+}
+ .publishedAt {
+	 margin-bottom: 40px;
+}
+ .post > h1 {
+	 font-size: 30px;
+	 font-weight: bold;
+	 margin: 40px 0 20px;
+	 background-color: #eee;
+	 padding: 10px 20px;
+	 border-radius: 5px;
+}
+ .post > h2 {
+	 font-size: 24px;
+	 font-weight: bold;
+	 margin: 40px 0 16px;
+	 border-bottom: 1px solid #ddd;
+}
+ .post > p {
+	 line-height: 1.8;
+	 letter-spacing: 0.2px;
+}
+ .post > ol {
+	 list-style-type: decimal;
+	 list-style-position: inside;
+}
+</style>
